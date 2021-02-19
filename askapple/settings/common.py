24cbe5e6ae10a0bc9bd3215de92eea6863964e81
noname2048 <sungwook.csw@gmail.com>
2021-02-19 09:26:00 +0900

    django rest framework added

--- askapple/settings/common.py
+++ askapple/settings/common.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd
+    "rest_framework",
+    # local
 ]
 
 MIDDLEWARE = [
